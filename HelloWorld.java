47e1cb9a95d70c5a67615950ddc8a3a26fd496bf
ALice Nan <10255102503@stu.ecnu.edu.cn>
2026-03-26 19:53:25 +0800

    change HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWorld{
-   public static void main(Sting[] args){
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("HelloWorld");
-   }
+        System.out.println("HelloWorld again!");
+    }
 }
